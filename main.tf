aa9af44a09663e3b7f32c84ab41a0bf1d98ce12b
Par <pacroy@users.noreply.github.com>
2025-01-04 15:02:06 +0700

    Upgrade azurerm from 3.56 to 4.14 + skip register resource provider

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.56.0"
+      version = "~> 4.14.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -13,6 +13,7 @@ terraform {
 }
 
 provider "azurerm" {
+  resource_provider_registrations = "none"
   features {}
 }
 
